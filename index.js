abdd0b39145a0576668cb6884603bdab68ec0eb2
Taboichebet <taboibrenda@gmail.com>
2022-08-24 21:18:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.querySelector('input');
+    function addingEventListener() {
+    }
+        input.addEventListener('click',e => alert('clicked!'));
 }
+
+	
+
